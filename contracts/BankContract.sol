348be7fbd993f3afcf16833407bd3c2ca10d576e
Durga Prasad Vallamdas <134387189+prasadvallamdas@users.noreply.github.com>
2024-07-02 18:26:55 +0530

    BankContract.sol

--- contracts/BankContract.sol
+++ contracts/BankContract.sol
@@ -1,1 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.4.0 <= 0.9.0;
 
+contract BankContract {
+    string public users;
+    string public bankaccount;
+    string public history;
+
+    function addhistory(string memory ra) public{
+        history = ra;
+    }
+
+    function gethistory() public view returns (string memory){
+        return history;
+    }
+
+    function addUsers(string memory u) public {
+        users = u;	
+    }
+
+    function getUsers() public view returns (string memory) {
+        return users;
+    }
+
+    function bankAccount(string memory ba) public {
+        bankaccount = ba;	
+    }
+
+    function getBankAccount() public view returns (string memory) {
+        return bankaccount;
+    }
+
+  
+
+
+    constructor() public {
+        users = "empty";
+	bankaccount = "empty";
+    history = "empty";
+    }
+}
